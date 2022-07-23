d68065e0037ae27c7fa80e602958bf9757686b6f
tamachika <62124334+tamachika@users.noreply.github.com>
2022-07-23 09:48:27 +0900

    Add files via upload

---
+++ report1.c
@@ -0,0 +1,204 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void pa() {
+printf(" ｎ∩ｎ\n");
+printf(" ||||ｈ\n");
+printf(" |||||| \n");
+printf("∩ ￣￣ |\n");
+printf("|　￣　|\n");
+printf("ヽ )　ノ\n");
+return;
+}
+
+void pa_c() {
+printf("_______________________\n");
+printf("|         ｎ∩ｎ          |\n");
+printf("|         ||||h       |\n");
+printf("|         ||||||       |\n");
+printf("|        ∩ ￣￣ |        |\n");
+printf("|        |　￣　|       |\n");
+printf("|        ヽ )　ノ       |\n");
+printf("|                      |\n");
+printf("|______________________|\n");
+printf("         |      |\n");
+printf("         |      |\n");
+printf("    -----------------\n");
+return;
+}
+
+void cho() {
+printf("　　　　　fヽ\n");
+printf("　　　　　| |\n");
+printf("　(~＼　　| |\n");
+printf("　 ＼ ＼　| |\n");
+printf("　　 ＼ ＼| |\n");
+printf("　　/￣＿　 ヽ\n");
+printf("　 /＼＿(￣￣ヽ\n");
+printf("　 ＼＿) ~７　|\n");
+printf("　　 ヽ　 ｜　|\n");
+printf("　　　 ＼　＼ |\n");
+printf("　　　　｜　　|\n");
+printf("　　　　/　　 |\n");
+return;
+}
+
+void cho_c() {
+printf(" __________________________\n");
+printf(" |      |___二二__으_|      |\n");
+printf("||                         ||\n");
+printf("||                         ||\n");
+printf(" |       　　　fヽ          ||\n");
+printf("||   　　　　　| |          ||\n");
+printf("||   　(~＼　　| |          |\n");
+printf(" |　     ＼ ＼　| |         |\n");
+printf(" |　　    ＼ ＼| |          |\n");
+printf(" |　　   /￣＿　 ヽ          |\n");
+printf(" |   　 /＼＿(￣￣ヽ         |\n");
+printf(" |　     ＼＿) ~７ |        |\n");
+printf(" |　　    　 ｜　  |         |\n");
+printf(" |　　　     /　　 |         |\n");
+printf(" |                         |\n");
+printf(" |                         |\n");
+printf(" |_________________________|\n");
+return;
+}
+
+void gu() {
+printf("  _ｒ1´ｌ│-､\n");
+printf("〈_LＵ∪  /´ \n");
+printf(" ヽ　　　ﾉ\n");
+printf("   i   |\n");
+return;
+}
+
+void gu_c() {
+printf("________________________\n");
+printf("|                       |\n");
+printf("|                       |\n");
+printf("|        _ｒ1´ｌ│-､      |\n");
+printf("|       〈_LＵ∪  /´     |\n");
+printf("|        ヽ　　　ﾉ        |\n");
+printf("|        i   |          |\n ");
+printf("|                       |\n ");
+printf("|                       |\n");
+printf("|                       |\n");
+printf("|                       |\n");
+printf("|_______________________|\n");
+printf("|___________으___________|\n");
+return;
+}
+
+void win() {
+printf("┌○┐\n");
+printf("│勝│ハハ\n");
+printf("│　│ﾟωﾟ)\n");
+printf("│利│ ／/\n");
+printf("└○┘ (⌒)\n");
+printf("　　し⌒￣\n");
+}
+
+void draw(){
+printf("■■■■■   ■■■■     ■   ■    ■   ■\n");
+printf("■   ■■  ■   ■    ■   ■■  ■■   ■\n");
+printf("■    ■  ■   ■   ■ ■   ■  ■■  ■ \n");
+printf("■    ■■ ■   ■   ■ ■   ■  ■ ■ ■ \n");
+printf("■    ■■ ■■■■   ■   ■  ■ ■  ■ ■ \n");
+printf("■    ■  ■  ■■  ■■■■■   ■■  ■■  \n");
+printf("■   ■■  ■   ■  ■   ■   ■■  ■■  \n");
+printf("■■■■■   ■    ■■     ■  ■    ■  \n");
+}
+
+void lose(){
+printf(" ＿|￣|○ 　負けた、、\n");
+}
+
+void cong(){
+printf("　.｜　　.／川＼　　｜ \n");
+printf("　　＼／┏┷┓＼／ \n");
+printf("　　 　 。┃勝┃｡ \n");
+printf("　　ﾞ　# ﾟ┃利┃; ｡ \n");
+printf("　　 ; ｡ ･┃E.┃･ # \n");
+printf("　　｡ ;ﾞ #┃N.┃#　｡ \n");
+printf("　　ﾞ･# : ┃D┃｡ ;　｡ \n");
+printf("　.;:#　ﾞ｡ﾟ┃!┃ﾟ ｡ # \n");
+printf("　; ｡;; ﾞ.:｡┗┯┛｡ # : # \n");
+printf(".　　　ﾊ,,ﾊ　 | \n");
+printf(".　　（ ﾟωﾟ ) | \n");
+printf("　　:/　　つΦ \n");
+}
+
+void game_over(){
+printf("　　　　　　　　　　　　 ／）\n");
+printf("　　　　　　　　　　　／／／）\n");
+printf("　　　　　　　　　 ／,.=ﾞ\'\'\"／\n");
+printf("　　　／　　　　 i f　,.r=\'\"-‐\'つ＿＿＿_　　ゲームオーバー？？　\n ");
+printf("　　/　　　　　 /　　　_,.-‐'~／⌒　　⌒＼        こまけぇこたぁいいんだよ！！\n");
+printf("　　　　／　 　,i　　　,二ﾆ⊃（ ●）.　（●）＼\n");
+printf("　　　/　 　　ﾉ　　　 ilﾞフ::::::⌒（__人__）⌒::::: ＼\n");
+printf("　　　　　　,ｲ｢ﾄ､　　,!,!|　　　　　|r┬-|　　　　　|\n");
+printf("　　　　　/　iﾄヾヽ_/ｨ\"＼ 　　 　 `ー'´ 　 　 ／\n");
+}
+
+void main()
+{
+int cpu,player,score=0,r=0,s=0,p=0;
+
+printf("<1:グー　２：チョキ　３：パー＞　\n勝ち＋２点　負け−１点　トータルで５点以上か−３点以下になると終わります\n＊終わりたい時は０を入力してください：");
+
+while (player != 0)
+{
+srand(time(0));
+cpu = rand()%3+1;
+scanf("%d", &player);
+
+if (player == 0) {
+    printf("ありがとうございました");
+    exit(0);
+} else if(player < 1 || 3 < player ){
+    printf("1~3以外は入力しないでください。０を入力すると終わります。\n");
+    printf("*********************************************************************\n");
+    continue;
+}
+
+switch (player)
+{
+case 1: gu();break;
+case 2: cho();break;
+case 3: pa();break;
+}
+
+switch (cpu)
+{
+case 1: gu_c();r++;break;
+case 2: cho_c();s++;break;
+case 3: pa_c();p++;break;
+}
+printf("cpuの出した数 グー%d  チョキ%d  パー%d\nスコア%d\n", r,s,p,score);
+
+if(cpu == player){
+    draw();
+} else if ( (player==1 && cpu==2) || (player==2 && cpu==3) || (player==3 && cpu==1) ) {
+    win();
+    score += 2;
+} else{ 
+    lose(); 
+    score--;
+}
+
+
+if (score > 4) {
+    cong();
+    exit(0);
+} else if (score < -2){
+    game_over();
+    exit(0);
+}
+
+
+
+}
+return;
+
+}
